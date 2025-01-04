45d7f991b18650f47b83ef155a84eec51a693d7b
Daven Austhine Sumagang <160004612+davenarchives@users.noreply.github.com>
2025-01-04 14:09:54 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let seconds = 0;
+let timerInterval;
+
+function startTimer() {
+  if (timerInterval) return;
+
+  timerInterval = setInterval(() => {
+    seconds++;
+    document.getElementById('timer').textContent = seconds;
+  }, 1000);
+}
+
+function resetTimer() {
+  clearInterval(timerInterval); 
+  timerInterval = null; D
+  seconds = 0;
+  document.getElementById('timer').textContent = seconds; 
+}
